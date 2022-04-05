035cc47cb7fd333d3274d9a3b6a063efcd7493a3
Lee Goddard <leegee@gmail.com>
2022-04-05 09:23:29 +0200

    tidy

--- src/client/BufferLoader.js
+++ src/client/BufferLoader.js
@@ -1,10 +1,4 @@
-// http://www.html5rocks.com/en/tutorials/webaudio/intro/js/buffer-loader.js
-// via http://www.html5rocks.com/en/tutorials/webaudio/intro/
-
 export default class BufferLoader {
-	bufferList = [];
-	loadCount = 0;
-
 	constructor(context) {
 		this.context = context;
 	}
@@ -16,13 +10,12 @@ export default class BufferLoader {
 
 		return new Promise((resolve, reject) => {
 			xhr.onload = () => {
-				console.debug('loaded audio');
 				this.context.decodeAudioData(xhr.response, (buffer) => {
-					return buffer ? resolve(buffer) : reject('error decoding file data: ' + url);
+					return buffer ? resolve(buffer) : reject(`BufferLoader error decoding file data from ${url}`);
 				});
 			}
 
-			xhr.onerror = () => reject(`BufferLoader XHR error: ${url}`);
+			xhr.onerror = () => reject(`BufferLoader XHR error from ${url}`);
 			xhr.send();
 		});
 	}
